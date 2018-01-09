82855209f9da845d7c7c0cd739fad1d9e6150369
Joseph Lansang <josephlansang@gmail.com>
2018-01-09 21:34:19 +0800

    PHP

---
+++ mod06-01/index.php
@@ -0,0 +1,65 @@
+<?php
+	// echo "Welcome to PHP programming!";
+
+	// phpinfo();
+
+	// $name = "Joseph Lansang";
+
+	// print "Good Morning, $name!";
+
+	// echo "today is " . date("F j, Y") . "."
+
+?>
+
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewpoint" content="width=device-width,initial-scale=1.0, maximum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<title>Welcome to PHP programming!</title>
+</head>
+<body>
+
+<?php 
+	//$msg = 'Welcome to PHP programming';
+	//echo "$msg";
+
+	// $username = 'Jane Smith';
+	// echo $username;
+	// echo '<br>';
+	// $current_user = $username;
+	// echo "$current_user";
+
+	// echo 'Buzz Lightyear once said: "You\'re a toy!"<br>';
+	// echo 'You deleted C:\\*.*<br>';
+	// echo "You deleted C:\*.*";
+
+	// echo 'This will not expand: \n a newline';
+
+	// $number1 = 150;
+	// $number2 = 33.5;
+	// $sum = $number1 + $number2;
+	// $product = $number1 * $number2;
+	// $difference = $number1 - $number2;
+	// $quotient = $number1 / $number2;
+	// echo "Sum is: $sum" . '<br>';
+	// echo "Product is: $product"  . '<br>';
+	// echo "Difference is: $difference"  . '<br>';
+	// echo "Quotient is: $quotient";
+
+	// $members = array('kato','shem','angeli','ali','carmela');
+	// $color = array('red','green','blue');
+	// $fruits = array('mango','apple','orange');
+	// echo "$members[1]<br>";
+	// echo "$color[2]<br>";
+	// echo "$fruits[2]<br>";
+	// echo "$fruits[2]<br>";
+
+	define("USER_NAME", "Joseph Lansang");
+	$current_user = USER_NAME;
+	echo $current_user;
+ ?>
+
+</body>
+</html>
